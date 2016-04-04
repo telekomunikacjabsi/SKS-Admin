7a561a921d6c0c2b931253e18c220fea9528712c
Grzesiek <Hallo>
2016-04-04 20:15:12 +0200

    interfejs - v1.0

---
+++ SKS-Admin/SKS-Admin/OknoGłówne.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SKS_Admin
+{
+    /// <summary>
+    /// Interaction logic for OknoGłówne.xaml
+    /// </summary>
+    public partial class OknoGłówne : Window
+    {
+        public OknoGłówne()
+        {
+            InitializeComponent();
+            ADD_LIST();
+        }
+
+        private void ADD_LIST()
+        {
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+            listBox.Items.Add("PC1");
+            listBox.Items.Add("PC2");
+        }
+    }
+}
